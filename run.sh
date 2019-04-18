e45b2c4d8029a20bd718845260717296d5216db2
Timothy Fallon <tfallon@mit.edu>
2019-04-18 17:41:10 -0400

    source conda before running on tak

--- run.sh
+++ run.sh
@@ -1,1 +1,7 @@
+if [[ `hostname` == "tak" ]]
+then
+source /lab/solexa_weng/testtube/miniconda3/bin/activate
 bsub -o out.log -e err.log "nextflow run Venetian-SIAM.nf -resume -with-trace -with-report -with-dag dag.svg"
+else
+echo "not implemented"
+fi
